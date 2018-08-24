88102549661d34dbf51551e2b4b56b8b11343e75
Unknown <timothee.monceaux@gmail.com>
2018-08-24 21:53:08 +0200

    Change the name of the home activty

--- app/src/main/java/timothee/chabadabada/HomeActivity.kt
+++ app/src/main/java/timothee/chabadabada/HomeActivity.kt
@@ -11,13 +11,13 @@ import java.util.*
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class HomePageActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     private fun getValue(): String {
         return possibleValues.get(Random().nextInt(possibleValues.size))
     }
 
     private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
-        text_button.text = getValue()
+        home_text_button.text = getValue()
         false
     }
 
@@ -30,7 +30,7 @@ class HomePageActivity : AppCompatActivity() {
         // Upon interacting with UI controls, delay any scheduled hide()
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
-        text_button.setOnTouchListener(mDelayHideTouchListener)
+        home_text_button.setOnTouchListener(mDelayHideTouchListener)
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
