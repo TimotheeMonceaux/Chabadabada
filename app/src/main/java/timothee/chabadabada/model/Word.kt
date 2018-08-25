33dd6609f67d46d8129321bf09fc2955f2937041
Unknown <timothee.monceaux@gmail.com>
2018-08-25 09:42:01 +0200

    Implement basic version of model class Word

--- app/src/main/java/timothee/chabadabada/model/Word.kt
+++ app/src/main/java/timothee/chabadabada/model/Word.kt
@@ -1,4 +1,18 @@
 package timothee.chabadabada.model
 
-class Word {
+import timothee.chabadabada.model.interfaces.IDeserializable
+import timothee.chabadabada.model.interfaces.ISerializable
+
+class Word(val word: String) : ISerializable {
+
+    override fun serialize(): String {
+        return word
+    }
+
+    companion object : IDeserializable {
+
+        override fun deserialize(json: String): ISerializable {
+            return Word(json)
+        }
+    }
 }
