386f29c1b36680a0a6daffba84b17657bc7bb9cb
Unknown <timothee.monceaux@gmail.com>
2018-08-26 15:01:00 +0200

    Implement timer

--- app/src/main/java/timothee/chabadabada/MainActivity.kt
+++ app/src/main/java/timothee/chabadabada/MainActivity.kt
@@ -4,13 +4,16 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MotionEvent
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 class MainActivity : AppCompatActivity() {
 
     var turn: Int = 0
     var nbTurns: Int = 0
+    var timer: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         // Set the Layout
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        // Map the listeners to the  Buttons
+        main_hourglass_button.setOnTouchListener(hourglassButtonTouchListener)
 
         nextTurn()
     }
@@ -41,6 +47,33 @@ class MainActivity : AppCompatActivity() {
             main_turn_counter_text.text = "TURN $turn / $nbTurns"
             main_first_card_text.text = getValue()
             main_second_card_text.text = getValue()
+            main_timer_text.visibility = View.GONE
+            main_hourglass_button.visibility = View.VISIBLE
+        }
+    }
+
+    private val hourglassButtonTouchListener = View.OnTouchListener { _, event ->
+        if (event.action == MotionEvent.ACTION_UP)
+            startTimer()
+        false
+    }
+
+    private fun startTimer() {
+        // Initialize the Timer View
+        timer = 301
+        main_hourglass_button.visibility = View.GONE
+        main_timer_text.visibility = View.VISIBLE
+
+        // Run the Timer and Timer Task
+        fixedRateTimer("hourglass-timer",true,0, 100) {
+            if (--timer <= 0) {
+                runOnUiThread {main_timer_text.text = "OVERTIME, CHANGING CARD IN ${5+timer/10}..."}
+                if (timer <= -50) {
+                    this.cancel()
+                    runOnUiThread {nextTurn()}
+                }
+            }
+            else runOnUiThread {main_timer_text.text = "${timer/10.0}s LEFT"}
         }
     }
 
