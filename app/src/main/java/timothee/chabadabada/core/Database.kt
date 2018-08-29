f35cc304d732d26c7420133abb29ec78438072d1
Unknown <timothee.monceaux@gmail.com>
2018-08-29 22:12:27 +0200

    Test implem of the core database component

--- app/src/main/java/timothee/chabadabada/core/Database.kt
+++ app/src/main/java/timothee/chabadabada/core/Database.kt
@@ -1,4 +1,39 @@
 package timothee.chabadabada.core
 
-class Database {
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import timothee.chabadabada.model.Word
+import timothee.chabadabada.model.dao.WordDao
+
+@Database(entities = arrayOf(Word::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun wordDao(): WordDao
+
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase? {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    // TODO go back to real database
+//                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+//                                                    AppDatabase::class.java,
+//                                                    "chabadabada.db")
+//                                   .build()
+                    INSTANCE = Room.inMemoryDatabaseBuilder(context.applicationContext,
+                                                    AppDatabase::class.java)
+                                   .build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance() {
+            INSTANCE = null
+        }
+    }
 }
