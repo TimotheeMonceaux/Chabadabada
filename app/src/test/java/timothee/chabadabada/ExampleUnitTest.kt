37f99b8e1c6610fdffd1c583d2dccf968d1f0d16
Unknown <timothee.monceaux@gmail.com>
2018-09-15 22:27:23 +0200

    Add serialization unit test

--- app/src/test/java/timothee/chabadabada/ExampleUnitTest.kt
+++ app/src/test/java/timothee/chabadabada/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package timothee.chabadabada
 import org.junit.Test
 
 import org.junit.Assert.*
+import timothee.chabadabada.model.Word
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,13 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+}
+
+class ModelUnitTest {
+    @Test
+    fun serialization_isCorrect() {
+        var word = Word("WORD")
+        assertEquals(word.word, Word.deserialize(word.serialize()).word)
     }
 }
