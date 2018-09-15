2431b18c6aa8e23684ae8165c5fc46a66f377f69
Unknown <timothee.monceaux@gmail.com>
2018-09-15 23:11:28 +0200

    Refact of the WordStreamer so that it is one singleton by language

--- app/src/main/java/timothee/chabadabada/core/WordStreamer.kt
+++ app/src/main/java/timothee/chabadabada/core/WordStreamer.kt
@@ -2,9 +2,11 @@ package timothee.chabadabada.core
 
 import android.content.Context
 import timothee.chabadabada.model.Word
+import timothee.chabadabada.model.raw.Language
+import java.security.InvalidParameterException
 import kotlin.coroutines.experimental.buildIterator
 
-class WordStreamer(val context: Context) {
+class WordStreamer private constructor(val context: Context, val language: Language) {
 
     val wordIterator: Iterator<Word> = getIterator()
 
@@ -16,5 +18,26 @@ class WordStreamer(val context: Context) {
 
     fun nextValue(): Word {
         return wordIterator.next()
+    }
+
+    companion object {
+        val INSTANCES: MutableMap<Language, WordStreamer?> = mutableMapOf(Language.French to null, Language.English to null)
+
+        fun getInstance(context: Context, language: Language): WordStreamer {
+            // Sanity check on the parameters
+            if (!INSTANCES.containsKey(language))
+                throw InvalidParameterException("Trying to access to an non-existing WordStreamer: $language")
+
+            if (INSTANCES[language] == null)
+                INSTANCES[language] = WordStreamer(context, language)
+            return INSTANCES[language]!!
+        }
+
+        fun destroyInstance(language: Language) {
+            // Sanity check on the parameters
+            if (!INSTANCES.containsKey(language))
+                throw InvalidParameterException("Trying to access to an non-existing WordStreamer: $language")
+            INSTANCES[language] = null
+        }
     }
 }
